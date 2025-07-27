540bdb9bf315e55dca99682635bbdcd1c860971f
Ilaria Casolino <ilary.casolino@gmail.com>
2025-07-27 16:55:31 +0200

    modifiche

---
+++ frontend/src/public/templates/register.js
@@ -0,0 +1,57 @@
+document.getElementById("registration-form").addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    const user_name = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const conferma_password = document.getElementById("conferma_password").value;
+
+    if(password !== conferma_password) {
+        document.getElementById("msg-error").innerText = "Le password non coincidono.";
+        document.getElementById("msg-error").style.display = "block";
+
+        setTimeout(() => {
+            document.getElementById("msg-error").style.display = "none";
+            document.getElementById("msg-error").innerText = "";
+        }, 3000);
+        return;
+    }
+
+    const data = {
+        user_name: user_name,
+        email: email,
+        password: password
+    };
+
+    try {
+        const response = await fetch("/register", {
+            method: "POST",
+            headers: {"Content-Type" : "application/json"},
+            body: JSON.stringify(data)
+        });
+
+        if (!response.ok) {
+            const errorData = await response.json();
+            document.getElementById("msg-error").innerText = errorData.detail;
+            document.getElementById("msg-error").style.display = "block";
+
+            setTimeout(() => {
+                document.getElementById("msg-error").style.display = "none";
+                document.getElementById("msg-error").innerText = "";
+            }, 3000);
+            return;
+        }
+        
+        window.location.href = "/login";
+
+    } catch (error) {
+        console.error("Errore nella fetch:", error);
+        document.getElementById("msg-error").innerText = "Errore durante la registrazione.";
+        document.getElementById("msg-error").style.display = "block";
+
+        setTimeout(() => {
+            document.getElementById("msg-error").style.display = "none";
+            document.getElementById("msg-error").innerText = "";
+        }, 3000);
+    }
+});
