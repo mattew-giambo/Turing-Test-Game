9bd8f8b25e6f91b73e08e72304202c253a860b6d
Ilaria Casolino <ilary.casolino@gmail.com>
2025-08-04 10:57:28 +0200

    modifiche

--- frontend/src/public/js/login.js
+++ frontend/src/public/js/login.js
@@ -3,34 +3,44 @@ document.getElementById("login-form").addEventListener("submit", async(e)=>{
 
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
-    const msg_error = document.getElementById("msg-error")
 
     const data = {
         user_name: username,
         password: password
     }
 
-    const response = await fetch("/login", {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(data)
-    });
+    try {
+        const response = await fetch("/login", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(data)
+        });
 
-    const response_data = await response.json();
-    if(response.ok){
+        const response_data = await response.json();
+        console.log("response_data.detail:", response_data.detail);
+
+        if (!response.ok) {
+            document.getElementById("msg-error").innerText = response_data.detail;
+            document.getElementById("msg-error").style.display = "block";
+
+            setTimeout(() => {
+                document.getElementById("msg-error").style.display = "none";
+                document.getElementById("msg-error").innerText = "";
+            }, 3000);
+            return;
+        }
+
         const url = new URL(`/profilo/${response_data.user_id}`, window.location.origin);
         url.searchParams.set("token", `${response_data.token}`);
         return window.location.href= url.toString();
-    }
-    else{
-        msg_error.innerText= response_data.detail;
-        msg_error.style.display = "block";
+    } catch (error) {
+        console.error("Errore nella fetch:", error);
+        document.getElementById("msg-error").innerText = "Errore durante il login.";
+        document.getElementById("msg-error").style.display = "block";
 
-        setTimeout(()=>{
-            msg_error.style.display = "none";
-            msg_error.innerText = "";
+        setTimeout(() => {
+            document.getElementById("msg-error").style.display = "none";
+            document.getElementById("msg-error").innerText = "";
         }, 3000);
-
-        return;
     }
 });
